2207ca6474a08d5a50f446d26de2736167d07273
Thomas Hillenbrand <tomhillenbrand16@gmail.com>
2020-11-21 19:16:09 -0600

    Added command objects for Recipe App data model

---
+++ src/main/java/guru/springframework/commands/IngredientCommand.java
@@ -0,0 +1,20 @@
+package guru.springframework.commands;
+
+import guru.springframework.domain.UnitOfMeasure;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class IngredientCommand {
+
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+    private UnitOfMeasure uom;
+
+}
---
+++ src/main/java/guru/springframework/commands/RecipeCommand.java
@@ -0,0 +1,33 @@
+package guru.springframework.commands;
+
+import guru.springframework.domain.Category;
+import guru.springframework.domain.Difficulty;
+import guru.springframework.domain.Ingredient;
+import guru.springframework.domain.Notes;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class RecipeCommand {
+
+    private Long id;
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+    private Set<IngredientCommand> ingredients = new HashSet<>();
+    private Byte[] image;
+    private NotesCommand notes;
+    private Difficulty difficulty;
+    private Set<CategoryCommand> categories = new HashSet<>();
+
+}

74cf6188320a01713cbd8333a5b85d0ea8064b51
Thomas Hillenbrand <tomhillenbrand16@gmail.com>
2020-12-05 22:47:39 -0600

    Added basic image upload capability

---
+++ src/main/java/guru/springframework/controllers/ImageController.java
@@ -0,0 +1,39 @@
+package guru.springframework.controllers;
+
+import guru.springframework.services.ImageService;
+import guru.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@Controller
+public class ImageController {
+
+    private final ImageService imageService;
+    private final RecipeService recipeService;
+
+    public ImageController(ImageService imageService, RecipeService recipeService) {
+        this.imageService = imageService;
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping("recipe/{recipeId}/image")
+    public String showUploadForm(@PathVariable String recipeId, Model model) {
+        model.addAttribute("recipe", recipeService.getRecipeCommandById(Long.valueOf(recipeId)));
+        return "recipe/imageuploadform";
+    }
+
+    @PostMapping("/recipe/{recipeId}/image")
+    public String handleImagePost(@PathVariable String recipeId, @RequestParam("imagefile") MultipartFile file) {
+
+        imageService.saveImageFile(Long.valueOf(recipeId), file);
+
+        return "redirect:/recipe/"+recipeId+"/show";
+    }
+}
---
+++ src/test/java/guru/springframework/controllers/ImageControllerTest.java
@@ -0,0 +1,73 @@
+package guru.springframework.controllers;
+
+import guru.springframework.commands.RecipeCommand;
+import guru.springframework.services.ImageService;
+import guru.springframework.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.multipart.MultipartFile;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class ImageControllerTest {
+
+    @Mock
+    ImageService imageService;
+    @Mock
+    RecipeService recipeService;
+
+    ImageController imageController;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        imageController = new ImageController(imageService, recipeService);
+        mockMvc = MockMvcBuilders.standaloneSetup(imageController).build();
+    }
+
+    @Test
+    void getImageFormTest() throws Exception {
+        // given
+        RecipeCommand command = new RecipeCommand();
+        command.setId(1L);
+
+        // when
+        when(recipeService.getRecipeCommandById(anyLong())).thenReturn(command);
+        mockMvc.perform(get("/recipe/1/image"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("recipe"));
+
+        // then
+        verify(recipeService, times(1)).getRecipeCommandById(anyLong());
+    }
+
+    @Test
+    void handleImagePostTest() throws Exception {
+        MockMultipartFile multipartFile =
+                new MockMultipartFile("imagefile", "testing.txt", "text/plain", "Spring Framework".getBytes());
+
+        mockMvc.perform(multipart("/recipe/1/image").file(multipartFile))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location", "/recipe/1/show"));
+
+        verify(imageService, times(1)).saveImageFile(anyLong(), any(MultipartFile.class));
+    }
+
+
+}
